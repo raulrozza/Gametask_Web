7c541d56e495a3d150d9badde68d94cbea24a2cb
Raul Rosá <raul.hmetal@gmail.com>
2020-05-25 10:56:10 -0300

    New feature: Ranking skeleton and dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-// import { Container } from './styles';
+// Custom Components
+import Loading from '../../components/Loading';
+import Ranking from '../../components/Ranking';
+
+// Services
+import api from '../../services/api';
+import getToken from '../../services/getToken';
+
+import './styles.css';
 
 const Dashboard = () => {
-  return <h1>Dashboardzinho</h1>;
+  const [game, setGame] = useState({});
+  const [loading, setLoading] = useState(true);
+  const history = useHistory();
+
+  useEffect(() => {
+    (async () => {
+      try{
+        const userInfo = getToken();
+        if(!userInfo)
+          history.push('/');
+
+        const {data: game} = await api.get('/game/5ebc0a1e1da3fa28f4a455a7', {
+          headers: {
+            Authorization: 'Bearer '+userInfo.token,
+          }
+        });
+
+        setGame(game);
+        setLoading(false);
+      }
+      catch(error){
+        const { response: { data } } = error;
+        console.error(error);
+
+        if(data.error === "TokenExpiredError: jwt expired"){
+          localStorage.removeItem('loggedUser');
+          history.push('/')
+        }
+      }
+    })();
+  }, [history]);
+
+  if(loading)
+    return <Loading />;
+  return (
+    <main className="dashboard">
+      <Ranking ranking={game.weeklyRanking} />
+      <div className="column">
+        <Ranking ranking={game.weeklyRanking} />
+        <Ranking ranking={game.weeklyRanking} />
+      </div>
+      <Ranking ranking={game.weeklyRanking} />
+    </main>
+  );
 }
 
 export default Dashboard;
