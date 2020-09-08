5169c96cba0b816f48e9be8ee04a0d172401d952
Raul Rosá <raul.hmetal@gmail.com>
2020-09-08 10:30:20 -0300

    Fix: Modal no longer supress children click.

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -16,21 +16,19 @@ const Modal: React.FC<ModalProps> = ({
   size = 'md',
 }) => {
   const backgroundRef = useRef<HTMLDivElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (backgroundRef.current)
-      backgroundRef.current.addEventListener('click', () => closeModal());
-
-    if (containerRef.current)
-      containerRef.current.addEventListener('click', (event: MouseEvent) =>
-        event.stopPropagation(),
-      );
+      backgroundRef.current.addEventListener('click', function (
+        event: MouseEvent,
+      ) {
+        if (event.target && event.target === this) closeModal();
+      });
   }, [closeModal]);
 
   return (
     <Background ref={backgroundRef}>
-      <Container size={size} ref={containerRef}>
+      <Container size={size}>
         <div className="modal-title">
           <h2>{title}</h2>
           <button className="close" type="button" onClick={closeModal}>
