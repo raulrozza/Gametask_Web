a1e722234be3dcb5e0ae56dd81197e1aeea69fe7
Raul <raul.hmetal@gmail.com>
2021-02-28 10:44:02 -0300

    Edit: Added getGameDetails service

---
+++ src/modules/dashboard/services/GetGameDetailsService.ts
@@ -0,0 +1,22 @@
+import IGame from 'shared/entities/IGame';
+import IGamesRepository from 'shared/repositories/IGamesRepository';
+
+interface IExecute {
+  game?: IGame;
+  error?: string;
+  shouldLogout?: boolean;
+}
+
+export default class GetGameDetailsService {
+  constructor(private gamesRepository: IGamesRepository) {}
+
+  public async execute(): Promise<IExecute> {
+    try {
+      const game = await this.gamesRepository.findOne();
+
+      return { game };
+    } catch (error) {
+      return { error: error.message, shouldLogout: error.shouldLogout };
+    }
+  }
+}
