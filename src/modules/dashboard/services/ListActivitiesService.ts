a30be5e97b2b1af6c4fe68b5b1197e9f44babae6
Raul <raul.hmetal@gmail.com>
2021-03-02 15:09:59 -0300

    Edit: Added list activities service

---
+++ src/modules/dashboard/services/ListActivitiesService.ts
@@ -0,0 +1,22 @@
+import IActivitiesRepository from 'modules/dashboard/repositories/IActivitiesRepository';
+import IActivity from 'modules/dashboard/entities/IActivity';
+
+interface IExecute {
+  activities?: IActivity[];
+  error?: string;
+  shouldLogout?: boolean;
+}
+
+export default class ListActivitiesService {
+  constructor(private activitiesRepository: IActivitiesRepository) {}
+
+  public async execute(): Promise<IExecute> {
+    try {
+      const activities = await this.activitiesRepository.findAll();
+
+      return { activities };
+    } catch (error) {
+      return { error: error.message, shouldLogout: error.shouldLogout };
+    }
+  }
+}
