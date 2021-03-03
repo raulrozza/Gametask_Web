754f560d68fbb428b8d87cc0e978ddc93e4d7627
Raul <raul.hmetal@gmail.com>
2021-03-03 16:40:06 -0300

    Edit: Added basic inputs on achievement form

--- src/modules/dashboard/view/pages/Achievements/components/AchievementEditor/index.tsx
+++ src/modules/dashboard/view/pages/Achievements/components/AchievementEditor/index.tsx
@@ -1,14 +1,57 @@
 import React from 'react';
+import { Formik } from 'formik';
+import AchievementSchema from 'modules/dashboard/validation/AchievementSchema';
+import { Button, ImageInput, Input, Textarea } from 'shared/view/components';
 
-import { Container } from './styles';
+import { ButtonContainer, Container, Form } from './styles';
 
 interface AchievementEditorProps {
   visible: boolean;
   closeEditor: () => void;
 }
 
+interface IFormValues {
+  name: string;
+  description: string;
+  title: string;
+  image?: string | File;
+}
+
+const initialValues: IFormValues = {
+  name: '',
+  description: '',
+  title: '',
+  image: '',
+};
+
 const AchievementEditor: React.FC<AchievementEditorProps> = ({ visible }) => {
-  return <Container $visible={visible} />;
+  return (
+    <Container $visible={visible}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={AchievementSchema}
+        onSubmit={console.log}
+      >
+        {() => (
+          <Form>
+            <ImageInput name="image" fullWidth />
+
+            <Input name="name" placeholder="Nome da conquista" fullWidth />
+
+            <Textarea
+              name="description"
+              placeholder="Descreva como obter esta conquista"
+              fullWidth
+            />
+
+            <ButtonContainer>
+              <Button type="submit">Enviar</Button>
+            </ButtonContainer>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
 };
 
 export default AchievementEditor;
--- src/modules/dashboard/view/pages/Achievements/components/AchievementEditor/styles.ts
+++ src/modules/dashboard/view/pages/Achievements/components/AchievementEditor/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { Form as FormikForm } from 'formik';
 
 interface ContainerProps {
   $visible: boolean;
@@ -9,13 +10,15 @@ export const Container = styled.div<ContainerProps>`
     css`
       width: 0px;
 
+      overflow: hidden;
+
       transition: width 0.2s;
 
       ${$visible
         ? css`
             border-left: 1px solid ${theme.palette.primary.dark};
 
-            width: 420px;
+            width: 400px;
           `
         : ''}
 
@@ -23,4 +26,16 @@ export const Container = styled.div<ContainerProps>`
         border-left: none;
       }
     `}
+`;
+
+export const Form = styled(FormikForm)`
+  width: 100%;
+
+  padding: ${({ theme }) => theme.layout.spacing(3, 12)};
+`;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
 `;
