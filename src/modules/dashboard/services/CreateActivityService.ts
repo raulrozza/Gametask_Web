038a12288037b489a6eaecfdeaa05bb24c203066
Raul <raul.hmetal@gmail.com>
2021-03-10 16:11:21 -0300

    Edit: Added create activity service

---
+++ src/modules/dashboard/services/CreateActivityService.ts
@@ -0,0 +1,23 @@
+import ICreateActivityDTO from 'modules/dashboard/dtos/ICreateActivityDTO';
+import IActivity from 'modules/dashboard/entities/IActivity';
+import IActivitiesRepository from 'modules/dashboard/repositories/IActivitiesRepository';
+
+interface IExecute {
+  activity?: IActivity;
+  error?: string;
+  shouldLogout?: boolean;
+}
+
+export default class CreateActivityService {
+  constructor(private activitiesRepository: IActivitiesRepository) {}
+
+  public async execute(data: ICreateActivityDTO): Promise<IExecute> {
+    try {
+      const activity = await this.activitiesRepository.create(data);
+
+      return { activity };
+    } catch (error) {
+      return { error: error.message, shouldLogout: error.shouldLogout };
+    }
+  }
+}
