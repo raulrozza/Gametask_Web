87d12a457de113a1614848108e4fd3e607b13ee1
Raul <raul.hmetal@gmail.com>
2021-04-07 14:57:56 -0300

    Add: update on games repository

---
+++ src/modules/gameManagement/infra/repositories/GamesRepository.ts
@@ -0,0 +1,29 @@
+import IUpdateGameDTO from 'modules/gameManagement/dtos/IUpdateGameDTO';
+import IGamesRepository from 'modules/gameManagement/repositories/IGamesRepository';
+import { makeHttpProvider } from 'shared/container/providers';
+
+export default class GamesRepository implements IGamesRepository {
+  private httpProvider = makeHttpProvider();
+
+  public async update({
+    name,
+    image,
+    description,
+    primary,
+    secondary,
+  }: IUpdateGameDTO): Promise<void> {
+    await this.httpProvider.put('games', {
+      name,
+      description,
+      theme: {
+        primary,
+        secondary,
+      },
+    });
+
+    const data = new FormData();
+    data.append('image', image);
+
+    await this.httpProvider.patch('games/avatar', data);
+  }
+}
